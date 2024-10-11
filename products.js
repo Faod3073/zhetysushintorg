157133a6300d71d47c333ea8ada0c11ec6c4f6a1
Faod3073 <167752365+Faod3073@users.noreply.github.com>
2024-10-11 21:26:03 +0500

    Update products.js

--- products.js
+++ products.js
@@ -1,6 +1,6 @@
 const productsData = [
-    { id: 1, name: "H818", price: 19300, quantity: 4, img:"C:\Users\User\Downloads\Рисунок1.png"},
-    { id: 2, name: "I-1", price: 45200, quantity: 4, img:"C:\Users\User\Downloads\Рисунок2.png" },
+    { id: 1, name: "H818", price: 19300, quantity: 4, img:"Рисунок1.png"},
+    { id: 2, name: "I-1", price: 45200, quantity: 4, img:"Рисунок2.png" },
 ];
 
 // Get the products container element
